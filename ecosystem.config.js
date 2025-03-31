303132da4906997e34c4bb8341c7bea795842e0c
Kyle <lelegrau04@gmail.com>
2025-03-31 22:14:54 +0200

    ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     apps: [
         {
-            name: 'kylehub',
+            name: 'kylehub-neongear',
             script: 'node_modules/next/dist/bin/next',
             args: 'start',
             env: {
                 NODE_ENV: 'production',
-                PORT: 3000
+                PORT: 3001
             }
         }
     ]
